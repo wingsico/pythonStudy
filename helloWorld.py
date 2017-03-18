2fac58ebcb914bb2275338d597fce385cc6b31dd
wingsico <wingsico@outlook.com>
2017-03-19 00:35:16 +0800

    hello python!

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+print 'hello world!'
+print "hello again"
+print "I like typing this"
+print 'This is fun!'
+print 'Yay! Printing!'
+print "I'd much rather than 'not' "
+print 'i "said" do not touch this'
